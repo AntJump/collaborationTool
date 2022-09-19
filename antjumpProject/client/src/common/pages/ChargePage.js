551bcd14c0f865a56857446c8f2eb0a30bb9a57b
sanghak <yee950419@naver.com>
2022-09-19 14:28:45 +0900

    기타: 코드 저장

---
+++ antjumpProject/client/src/common/pages/ChargePage.js
@@ -0,0 +1,5 @@
+function ChargePage() {
+  return <div>요금제</div>;
+}
+
+export default ChargePage;
---
+++ antjumpProject/client/src/common/pages/MessengerPage.js
@@ -0,0 +1,5 @@
+function MessengerPage() {
+  return <div>채팅소개</div>;
+}
+
+export default MessengerPage;
