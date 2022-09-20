dac48cd1e55f6795403f4369f174eab24931ab7d
sanghak <yee950419@naver.com>
2022-09-20 12:50:51 +0900

    기타: 임시 커밋

--- antjumpProject/client/src/epics/project/pages/ProjectlistPage.js
+++ antjumpProject/client/src/epics/project/pages/ProjectlistPage.js
@@ -1,21 +1,19 @@
-
-
 import CreateButton from "../components/items/CreateButton";
 import MyProjectList from "../components/lists/MyProjectList";
 import ParticipatedProjectList from "../components/lists/ParticipatedProjectList";
 import CreateModal from "../components/modals/CreateModal";
 
 function ProjectlistPage() {
-    return (
-        <div>
-            <CreateModal/>
-            <h2>참여중인 프로젝트</h2>
-            <ParticipatedProjectList />
-            <h2>내 프로젝트</h2>
-            <CreateButton />
-            <MyProjectList/>
-        </div>
-    );
+  return (
+    <div>
+      <CreateModal />
+      <h2>참여중인 프로젝트</h2>
+      <ParticipatedProjectList />
+      <h2>내 프로젝트</h2>
+      <CreateButton />
+      <MyProjectList />
+    </div>
+  );
 }
 
 export default ProjectlistPage;
