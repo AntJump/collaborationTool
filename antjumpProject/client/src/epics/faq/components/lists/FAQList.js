288bd560b5bbce9a0cce8e08f89a746200171b43
syboosyboo <john782@ajou.ac.kr>
2022-09-20 13:17:08 +0900

    기능 추가 : FAQ,QNA 샘플 데이터 추가, Detail 이동 버튼 추가, module 수정

---
+++ antjumpProject/client/src/epics/faq/components/lists/FAQList.js
@@ -0,0 +1,124 @@
+import { faqRows } from "./FAQSample";
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
+import { GET_FAQS } from "../../../../modules/FAQModule";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+const Demo = styled('div')(({ theme }) => ({
+  backgroundColor: theme.palette.background.paper,
+}));
+
+function FAQList() {
+
+    const faq = useSelector(state => state.faqReducer);
+    const faqs = faq.list
+    console.log("faqs: ", faqs);
+
+    const dispatch = useDispatch();
+
+    useEffect(
+        ()=>{
+            dispatch({type: GET_FAQS, payload: faqRows});
+        },
+        [dispatch]
+    );
+
+    return faqs && (
+        <> 
+            <Box sx={{ flexGrow: 1, maxWidth: 1440 }}>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} md={3}/>
+                    <Grid item xs={12} md={3}>
+                        <Typography sx={{ mt: 4, mb: 2,
+                        fontFamily: 'monospace',
+                        fontWeight: 700 }} variant="h6" component="div" color='#004392' >
+                            개미 협업툴 사용 관련
+                        </Typography>
+                        <Demo>
+                                {
+                                    faqs.filter((faq) => faq.faqCategoryNo === 1)
+                                    .map((faq) =>(
+                                        <div key ={faq.id}>
+                                            <Button component={Link} to={faq.id}>
+                                                {faq.faqTitle}
+                                            </Button>
+                                        </div>
+                                    ))
+                                }
+                        </Demo>
+                    </Grid>
+                    <Grid item xs={12} md={2}/>            
+                    <Grid item xs={12} md={3}>
+                        <Typography sx={{ mt: 4, mb: 2,
+                        fontFamily: 'monospace',
+                        fontWeight: 700 }} variant="h6" component="div" color='#004392' >
+                            가격 정책
+                        </Typography>
+                        <Demo>
+                                {
+                                    faqs.filter((faq) => faq.faqCategoryNo === 2)
+                                    .map((faq) =>(
+                                        <div key ={faq.id}>
+                                            <Button component={Link} to={faq.id}>
+                                                {faq.faqTitle}
+                                            </Button>
+                                        </div>
+                                    ))
+                                }
+                        </Demo>
+                    </Grid>
+                </Grid>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} md={3}/>
+                    <Grid item xs={12} md={3}>
+                        <Typography sx={{ mt: 4, mb: 2,
+                        fontFamily: 'monospace',
+                        fontWeight: 700 }} variant="h6" component="div" color='#004392' >
+                            결제 관련
+                        </Typography>
+                        <Demo>
+                                {
+                                    faqs.filter((faq) => faq.faqCategoryNo === 3)
+                                    .map((faq) =>(
+                                        <div key ={faq.id}>
+                                            <Button component={Link} to={faq.id}>
+                                                {faq.faqTitle}
+                                            </Button>
+                                        </div>
+                                    ))
+                                }
+                        </Demo>
+                    </Grid>
+                    <Grid item xs={12} md={2}/>            
+                    <Grid item xs={12} md={3}>
+                        <Typography sx={{ mt: 4, mb: 2,
+                        fontFamily: 'monospace',
+                        fontWeight: 700 }} variant="h6" component="div" color='#004392' >
+                            관리자
+                        </Typography>
+                        <Demo>
+                                {
+                                    faqs.filter((faq) => faq.faqCategoryNo === 4)
+                                    .map((faq) =>(
+                                        <div key ={faq.id}>
+                                            <Button component={Link} to={faq.id}>
+                                                {faq.faqTitle}
+                                            </Button>
+                                        </div>
+                                    ))
+                                }
+                        </Demo>
+                    </Grid>
+                </Grid>
+            </Box>
+        </>
+    );
+}
+
+export default FAQList;
+
