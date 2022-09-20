f2d574287bc1fe7179e8e119e5cac9d711f77811
moon <sugaryfood@naver.com>
2022-09-20 15:51:13 +0900

    기능구현: 회원 리덕스 형태로 로그인/회원가입 변경 밒 토큰 유틸 파일 추가

--- antjumpProject/client/src/modules/index.js
+++ antjumpProject/client/src/modules/index.js
@@ -1,17 +1,17 @@
 import { combineReducers } from "redux";
 import qnaReducer from "./QNAModule";
 import paymentReducer from "./PaymentModule";
-
+import memberReducer from "./MemberModule";
 
 // 프로젝트 관련 모듈 import
 import { myProjectReducer, participatedProjectReducer } from "./ProjectModule";
 const rootReducer = combineReducers({
-    qnaReducer
-    ,paymentReducer
-    // 프로젝트 관련 모듈 combine
-    ,myProjectReducer
-    ,participatedProjectReducer
-
+  qnaReducer,
+  paymentReducer,
+  // 프로젝트 관련 모듈 combine
+  myProjectReducer,
+  participatedProjectReducer,
+  memberReducer,
 });
 
 export default rootReducer;
