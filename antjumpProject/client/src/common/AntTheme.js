1a5a2dc2e7f722dcbc0a052b705a708a3d1f676f
yunseo <cysdl5604@inu.ac.kr>
2022-09-22 13:00:01 +0900

    기능추가: 프로젝트 팀원 목록, 사이드바 반응형으로 변경

--- antjumpProject/client/src/common/AntTheme.js
+++ antjumpProject/client/src/common/AntTheme.js
@@ -1,7 +1,7 @@
 import { createTheme } from "@mui/material/styles";
 
 export const theme = createTheme({
-<<<<<<< HEAD
+
     palette: {
       button: {
         main: '#004392',
@@ -15,16 +15,5 @@ export const theme = createTheme({
         main: '#D9D9D9',
         contrastText: '#fff',
       },
-=======
-  palette: {
-    button: {
-      main: "#004392",
-      contrastText: "#fff",
->>>>>>> main
-    },
-    blackline: {
-      main: "#000",
-      contrastText: "#000",
-    },
   },
 });
