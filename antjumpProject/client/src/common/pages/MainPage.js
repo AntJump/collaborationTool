5d290bebb7928cb8648f2bd2ba508e2f1447da5c
moon <sugaryfood@naver.com>
2022-09-23 14:55:36 +0900

    기능추가: 회원 메인페이지에 로그인 화면 이동 링크 추가

--- antjumpProject/client/src/common/pages/MainPage.js
+++ antjumpProject/client/src/common/pages/MainPage.js
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 function MainPage() {
   return (
     <div>
       <h3>메인 페이지</h3>
+      <Link to="/members/login">
+        <h3>로그인</h3>
+      </Link>
     </div>
   );
 }
