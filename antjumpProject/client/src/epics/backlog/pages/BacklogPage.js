ba711113f1ab9e1294a43439af4e3cf4acdec5b6
yunseo <cysdl5604@inu.ac.kr>
2022-09-25 09:35:20 +0900

    기타: pull -> merge 완료

--- antjumpProject/client/src/epics/backlog/pages/BacklogPage.js
+++ antjumpProject/client/src/epics/backlog/pages/BacklogPage.js
@@ -1,16 +1,16 @@
 import { Grid } from "@mui/material";
 
-import { DndProvider } from 'react-dnd'
-import  {HTML5Backend} from 'react-dnd-html5-backend'
+// import { DndProvider } from 'react-dnd'
+// import  {HTML5Backend} from 'react-dnd-html5-backend'
 import BacklogTableIndex from "../components/BacklogTableIndex";
 function BacklogPage() {
   return (
 
-    <DndProvider backend={HTML5Backend}>
+    // <DndProvider backend={HTML5Backend}>
       <Grid container spacing={1} sx={{width:'90%' , margin:'auto'}}>
         <BacklogTableIndex/>
       </Grid>
-    </DndProvider>
+    // </DndProvider>
 
   );
 }
