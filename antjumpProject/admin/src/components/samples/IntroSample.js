49650eabe68384153cde9a26e5429d6eb14f9bb9
syboosyboo <john782@ajou.ac.kr>
2022-09-25 21:57:32 +0900

    기능 추가 : 관리자 FAQ, 결제, 문의, 소개, 목록, 상세, 글쓰기, 수정, 샘플 추가

---
+++ antjumpProject/admin/src/components/samples/IntroSample.js
@@ -0,0 +1,9 @@
+ 
+  export const introRows = [
+    { id: "1", introCategoryNo: 1, introCategoryName:'요금제', introTitle: '캘린더 동기화는 어떻게 하나요?', introContent: '잘하면됩니다.', introModifyDate: '2022-09-30'},
+    { id: "2", introCategoryNo: 2, introCategoryName:'프로젝트 협업', introTitle: '백로그와 스프린트가 뭔가요?', introContent: '잘하면됩니다.', introModifyDate: '2022-09-09' },
+    { id: "3", introCategoryNo: 3, introCategoryName:'일정 관리', introTitle: '필요한 일정 알림만 받고 싶어요', introContent: '잘하면됩니다.', introModifyDate: '2022-07-07' },
+    { id: "4", introCategoryNo: 4, introCategoryName:'메신저', introTitle: '비밀번호를 분실해서 다시 찾고 싶어요', introContent: '잘하면됩니다.', introModifyDate: '2022-08-15' },
+    { id: "5", introCategoryNo: 5, introCategoryName:'파일 다운로드', introTitle: '회원 탈퇴 관련 안내 사항', introContent: '잘하면됩니다.', introModifyDate: '2022-07-14' },
+
+];
--- antjumpProject/admin/src/pages/PaymentDetailPage.js
+++ antjumpProject/admin/src/pages/PaymentDetailPage.js
@@ -1,10 +1,85 @@
+import * as React from 'react';
+import { paymentRows } from "../components/samples/PaymentSample";
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import { GET_PAYMENT_DETAIL } from '../modules/PaymentModule';
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
+import { Button } from '@mui/material';
+import { Link } from "react-router-dom";
+
 function PaymentDetailPage() {
-    return (
-      <>
-      결제 상세
-      </>
+
+    
+    const { paymentNumber } = useParams();
+
+    const payments = useSelector(state => state.paymentReducer);
+    const payment = payments[paymentNumber-1];
+    console.log("payment: ", payments);
+
+    const dispatch = useDispatch();
+
+    useEffect(
+        ()=>{
+            dispatch({type: GET_PAYMENT_DETAIL, payload: paymentRows});
+        },
+        [dispatch]
     );
-  }
-  
-  export default PaymentDetailPage;
-  
+    return payment && (
+        <>
+        
+          <Box sx ={{mt: 15}}>
+            <Typography sx={{ mt: 1, mb: 1,
+                    fontFamily: 'monospace',
+                    fontWeight: 700 }} variant="h3" align='center' color='#004392' >
+                      결제 세부
+            </Typography>
+            <Box sx={{ml:10}} >
+                <Typography sx={{ mt: 1, mb: 1,
+                        fontFamily: 'monospace',
+                        fontWeight: 700 }} variant="h5" align='left' color='#004392' >
+                    회원 이메일 :  { payment.memberEmail }
+                </Typography>
+                <Typography sx={{ mt: 1, mb: 1,
+                        fontFamily: 'monospace',
+                        fontWeight: 700 }} variant="h5" align='left' color='#004392' >
+                    회원 핸드폰 번호 : { payment.memberPhone }
+                </Typography>
+                <Typography sx={{ mt: 1, mb: 1,
+                        fontFamily: 'monospace',
+                        fontWeight: 700 }} variant="h5" align='left' color='#004392' >
+                    회원 결제일자 : { payment.paymentTime }
+                </Typography>
+                <Typography sx={{ mt: 1, mb: 1,
+                        fontFamily: 'monospace',
+                        fontWeight: 700 }} variant="h5" align='left' color='#004392' >
+                    회원 결제키 : { payment.paymentKey }
+                </Typography>
+                <Typography sx={{ mt: 1, mb: 1,
+                        fontFamily: 'monospace',
+                        fontWeight: 700 }} variant="h5" align='left' color='#004392' >
+                    결제금액 : { payment.paymentAmount }
+                </Typography>
+                <Typography sx={{ mt: 1, mb: 1,
+                        fontFamily: 'monospace',
+                        fontWeight: 700 }} variant="h5" align='left' color='#004392' >
+                    환불 요청 여부 : { payment.refund }
+                </Typography>
+                <Typography sx={{ mt: 1, mb: 1,
+                      fontFamily: 'monospace',
+                      fontWeight: 700 }} variant="h5" align='left' color='#004392' >
+                        환불 요청 사유 : { payment.refundReason }
+                </Typography>
+            </Box>
+            <Box sx={{
+                    display: 'flex',
+                    justifyContent: 'center'
+                }}>
+            </Box>
+          </Box>
+        </>
+    );
+}
+
+export default PaymentDetailPage;
