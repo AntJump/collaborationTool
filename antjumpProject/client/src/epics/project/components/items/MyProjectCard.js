1a7318ce3f1534147f2a0948c270a77b810bb8c6
sanghak <yee950419@naver.com>
2022-09-25 09:40:31 +0900

    기타: issue 코드 merge

--- antjumpProject/client/src/epics/project/components/items/MyProjectCard.js
+++ antjumpProject/client/src/epics/project/components/items/MyProjectCard.js
@@ -1,18 +1,31 @@
-<<<<<<< HEAD
 import TableCell from "@mui/material/TableCell";
 import TableRow from "@mui/material/TableRow";
-import { Link } from "react-router-dom";
 import ProjectMenuButton from "./ProjectMenuButton";
 
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { SET_SELECTED_PROJECT } from "../../../../modules/ProjectModule";
+
 function MyProjectCard({ project }) {
-  console.log("projectCard props : ", project);
+  const dispatch = useDispatch();
+  const onClickProject = () => {
+    console.log(project);
+    dispatch({ type: SET_SELECTED_PROJECT, payload: project });
+  };
+
   return (
     <>
       <TableRow sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
         <TableCell component="th" scope="row" align="center">
-          {project.key}
+          {" "}
+          {project.key}{" "}
         </TableCell>
-        <TableCell component={Link} to={`/project/${project.id}`} align="left">
+        <TableCell
+          component={Link}
+          to={`/project/${project.id}`}
+          onClick={onClickProject}
+          align="left"
+        >
           {project.name}
         </TableCell>
         <TableCell align="center">{project.produceTime}</TableCell>
@@ -24,39 +37,6 @@ function MyProjectCard({ project }) {
       </TableRow>
     </>
   );
-=======
-import TableCell from '@mui/material/TableCell';
-import TableRow from '@mui/material/TableRow';
-import ProjectMenuButton from './ProjectMenuButton';
-
-import { Link } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { SET_SELECTED_PROJECT } from '../../../../modules/ProjectModule';
-
-function MyProjectCard({project}) {
-    
-    const dispatch = useDispatch();
-    const onClickProject = () => {
-        console.log(project);
-        dispatch({type: SET_SELECTED_PROJECT, payload: project})
-    }
-
-    return (
-        <>
-            <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0  } }}>
-                <TableCell component="th" scope="row" align='center'> {project.key} </TableCell>
-                <TableCell component={Link} to={`/project/${project.id}`} onClick={onClickProject} align="left" >{project.name}</TableCell>
-                <TableCell align="center" >{project.produceTime}</TableCell>
-                <TableCell align="center" >{project.endDate}</TableCell>
-                <TableCell align="center">{project.constructor}</TableCell>
-                <TableCell align="right"><ProjectMenuButton project={project}/></TableCell>
-            </TableRow>
-        
-        </>
-
-
-    );
->>>>>>> main
 }
 
 export default MyProjectCard;
