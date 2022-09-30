393ffc9bb4b16ac7c68b9db10cb5cfa794d11868
syboosyboo <john782@ajou.ac.kr>
2022-09-30 17:58:32 +0900

    버그 : 프로젝트 Component 폴더 대소문자 버그 해결

---
+++ antjumpProject/client/src/epics/project/components/containers/ProjectInputForm.js
@@ -0,0 +1,58 @@
+import TextField from '@mui/material/TextField';
+import { Box } from '@mui/system';
+import { Grid } from '@mui/material';
+import FinishButton from '../items/FinishButton';
+
+
+function ProjectInputForm(){
+
+    const onSubmitHandler = (e) =>{
+       
+    }
+
+    const onClickHandler = ()=>{
+        alert('프로젝트 생성 완료!');
+    }
+
+    return (
+
+        <form onSubmit={onSubmitHandler}>
+            <Box sx={{width:'90%', margin:'auto'}}>
+                <Grid container rowSpacing={4}>
+                    <Grid item xs={12}>
+                        <TextField 
+                            name="name" 
+                            variant="outlined" 
+                            label="프로젝트 이름 *"
+                            fullWidth
+                        /><br/>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField 
+                            name="summary" 
+                            variant="outlined" 
+                            label="프로젝트 요약 *"
+                            fullWidth
+                            rows={3}
+                        /><br/>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField 
+                            name="explanation" 
+                            variant="outlined" 
+                            label="프로젝트 설명"
+                            multiline
+                            rows={9}
+                            fullWidth
+                        /><br/>
+                    </Grid>
+                    <FinishButton text={"생성"} onClickHandler={onClickHandler}/>
+                </Grid>
+            </Box>
+            
+        </form>
+
+    );
+}
+
+export default ProjectInputForm;
