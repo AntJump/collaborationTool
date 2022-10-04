3c49f478841ed3518f2dba9a9bf1b16d6316fcf6
yunseo <cysdl5604@inu.ac.kr>
2022-10-04 09:18:17 +0900

    기능추가: 프로젝트 epic 폴더 생성

---
+++ antjumpProject/server/ant/src/main/java/com/antjump/ant/project/controller/ProjectController.java
@@ -0,0 +1,15 @@
+package com.antjump.ant.project;/**
+ * <pre>
+ * Class : ProjectController
+ * Comment: 클래스에 대한 간단 설명
+ * History
+ * ================================================================
+ * DATE             AUTHOR           NOTE
+ * ----------------------------------------------------------------
+ * 2022-10-03       최윤서           최초 생성
+ * </pre>
+ * @version 1
+ * @author 최윤서
+ * @see 참고할 class 또는 외부 url
+ */public class ProjectController {
+}
